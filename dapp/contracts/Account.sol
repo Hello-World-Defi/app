752646d4aed16a5456a678beb110d051a6c49fda
Nate Schultz <ngs32@drexel.edu>
2021-05-17 19:35:42 -0400

    got get/set to work without js

--- dapp/contracts/Account.sol
+++ dapp/contracts/Account.sol
@@ -1,7 +1,16 @@
 pragma solidity ^0.8.4;
 
-contract Account {
+contract AccountHelp {
     uint private data;
     function setData(uint a) public { data = a; }
     function getData() public view returns(uint) { return data; }
+}
+
+contract Account {
+    function setData(uint a) public {}
+    function getData() public returns(uint) {
+        AccountHelp a = new AccountHelp();
+        a.setData(10);
+        return a.getData();
+    }
 }
--- dapp/migrations/3_account.js
+++ dapp/migrations/3_account.js
@@ -1,7 +1,9 @@
 const deployInfo = require('../helpers/deployInfo');
 const Account = artifacts.require('Account');
+const AccountHelp = artifacts.require('AccountHelp');
 
 module.exports = async deployer => {
   await deployer.deploy(Account);
+  await deployer.deploy(AccountHelp);
   return deployInfo(deployer, Account);
 };
