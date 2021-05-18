7ced938dad57f331786e7202438d4e6bfdcd81a1
Nate Schultz <ngs32@drexel.edu>
2021-05-17 20:33:37 -0400

    got get/set working; removed voting stuff

--- dapp/contracts/Account.sol
+++ dapp/contracts/Account.sol
@@ -1,16 +1,7 @@
 pragma solidity ^0.8.4;
 
-contract AccountHelp {
+contract Account {
     uint private data;
     function setData(uint a) public { data = a; }
     function getData() public view returns(uint) { return data; }
-}
-
-contract Account {
-    function setData(uint a) public {}
-    function getData() public returns(uint) {
-        AccountHelp a = new AccountHelp();
-        a.setData(10);
-        return a.getData();
-    }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@
 import * as React from 'react';
 import { Container } from 'reactstrap';
 import fetchContracts from '../helpers/fetchContracts';
-import Account from '../helpers/Account';
+// import Account from '../helpers/Account';
 import './App.css';
 
 type Props = { network: string };
@@ -17,24 +17,27 @@ class App extends React.Component<Props, State> {
   constructor(props: { network: string }) {
     super(props);
     this.state = {
+      Account: null,
       balance: '',
     };
 
     this.getData = this.getData.bind(this);
     this.setData = this.setData.bind(this);
+    this.loadContract = this.loadContract.bind(this);
+  }
+
+  async loadContract(): any {
+    const { contracts } = await fetchContracts(this.props.network, ['Account']);
+    this.setState({ Account: contracts.Account, balance: 'loaded' });
   }
 
   async getData(): any {
     const bal = await this.state.Account.getData();
-    console.log(bal);
     this.setState({ balance: bal.toString() });
   }
 
   async setData(): any {
-    const { contracts } = await fetchContracts(this.props.network, ['Account']);
-    const acc = new Account(contracts.Account);
-    await acc.setData(123);
-    this.setState({ Account: acc, balance: 'set' });
+    await this.state.Account.setData(123);
   }
 
   render() {
@@ -42,8 +45,9 @@ class App extends React.Component<Props, State> {
       <Container>
         <h1>Hello World DeFi!</h1>
         <p>{this.state.balance}</p>
-        <button onClick={this.setData}>Set Account</button>
-        <button onClick={this.getData}>Get Balance</button>
+        <button onClick={this.loadContract}>Load Contract</button>
+        <button onClick={this.setData}>Set Data</button>
+        <button onClick={this.getData}>Get Data</button>
       </Container>
     );
   }
